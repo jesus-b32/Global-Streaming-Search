340184353f6eb63291de94c3d667fd2aff06122d
jesus <149453045+jesus-b32@users.noreply.github.com>
2024-05-29 22:39:26 -0700

    improved removing movies and TV shows from watchlist using AJAX

--- app/static/app.js
+++ app/static/app.js
@@ -1,19 +1,29 @@
 // JS script using Tom Select Plugin for selecting country or providers in detail page
-new TomSelect('#country', {
-    maxOptions: null,
-    sortField: {
-        field: "text",
-        direction: "asc"
-    }
-});
 
-new TomSelect('#streamingProvider', {
-    maxOptions: null,
-    sortField: {
-        field: "text",
-        direction: "asc"
-    }
-});
+const countries = document.querySelector('#country');
+const streamingProviders = document.querySelector('#streamingProvider');
+
+// Only creat select list if select id exist
+if(countries) {
+    new TomSelect('#country', {
+        maxOptions: null,
+        sortField: {
+            field: "text",
+            direction: "asc"
+        }
+    });
+}
+
+if (streamingProviders) {
+    new TomSelect('#streamingProvider', {
+        maxOptions: null,
+        sortField: {
+            field: "text",
+            direction: "asc"
+        }
+    });
+}
+
 
 
 // Enabling tootips for bootstrap
@@ -61,11 +71,11 @@ if (addBtn) {
 ///////////////////////////////////////////////////////////////////////////////////
 
 
-/** Remove movie or TV show to watchlist user clicks on add to watchlist button.
+/** Remove movie or TV show to watchlist user clicks on add to watchlist button in movie or TV show detail page.
  *  Submit a POST request.
  *  Update button to signify movie or TV show is no longer in watchlist
  *  */
-async function removeFromWatchlist (e) {
+async function removeFromWatchlistDetail (e) {
     e.preventDefault();
 
     if(!(e.target.classList.contains('watchlist_remove'))) {
@@ -75,6 +85,8 @@ async function removeFromWatchlist (e) {
     // get tmdb_id and media_type from data attribute in title header
     const tmdbId = document.querySelector('.video_title').dataset.tmdbId;
     const mediaType = document.querySelector('.video_title').dataset.mediaType;
+
+    console.log('target in watchlist: ', e.target);
 
     await fetch('/watchlist/remove', {
         method: 'POST',
@@ -93,14 +105,57 @@ async function removeFromWatchlist (e) {
     e.target.classList.add('btn-primary');
     e.target.firstElementChild.classList.add('fa-regular');
 }
-const removeBtn = document.querySelector('.watchlist_remove');
+
+
+/** Remove movie or TV show to watchlist user clicks on add to watchlist button in watchlist page.
+ *  Submit a POST request.
+ *  Update button to signify movie or TV show is no longer in watchlist
+ *  */
+async function removeFromWatchlistPage (e) {
+    e.preventDefault();
+
+    console.log('Target: ', e.target);
+    if(!(e.target.classList.contains('watchlist_remove_watchlist'))) {
+        return
+    }
+
+    // get tmdb_id and media_type from data attribute in title header
+    const tmdbId = e.target.dataset.tmdbId;
+    const mediaType = e.target.dataset.mediaType;
+
+    console.log('tmdb: ', tmdbId);
+    console.log('mediaType: ', mediaType);
+
+    await fetch('/watchlist/remove', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json; charset=utf-8'},
+        body: JSON.stringify({
+            tmdb_id: tmdbId,
+            media_type: mediaType
+        })
+    })
+
+    //remove video that user selected from webpage
+    const video = document.querySelector(`#${mediaType}-${tmdbId}`);
+    video.remove();
+}
+// remove button from movie or TV show details page
+const removeBtnDetails = document.querySelector('.watchlist_remove_detail');
+
+// container for watchlist in watchlist page
+const watchlistContainer = document.querySelector('#watchlist_container');
 
 // Only run eventlistener if remove button exist
-if (removeBtn) {
-    removeBtn.addEventListener('click', removeFromWatchlist);
+if (removeBtnDetails) {
+    removeBtnDetails.addEventListener('click', removeFromWatchlistDetail);
+}
+
+if (watchlistContainer) {
+    watchlistContainer.addEventListener('click', removeFromWatchlistPage);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
+
 
 
 //toggeling the display of streaming data by country or provider
@@ -123,5 +178,7 @@ function streaming_data_display(e) {
     }
 }
 
-visibility_selection.addEventListener('click', streaming_data_display);
+if (visibility_selection) {
+    visibility_selection.addEventListener('click', streaming_data_display);
+}
 
